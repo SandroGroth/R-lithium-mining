ef17f9eb447f171481697452762564ffe605c498
SandroGroth <sandro_groth@outlook.com>
2019-03-05 23:17:35 +0100

    Added NDWI function.

---
+++ func/ndwi.R
@@ -0,0 +1,21 @@
+ndwi <- function(band_folder, sensor_type="") {
+  
+  if (length(list.files(band_folder, pattern = "LT05|LT04")) != 0 | sensor_type == "l5") {
+    b_green <- brick(list.files(band_folder, pattern = "B2.TIF", full.names = TRUE))
+    b_nir <- brick(list.files(band_folder, pattern = "B4.TIF", full.names = TRUE))
+    message("Sensor mode: LT05/LT04")
+  } else if (length(list.files(band_folder, pattern = "LC08")) != 0 | sensor_type =="l8") {
+    b_green <- brick(list.files(band_folder, pattern = "B3.TIF", full.names = TRUE))
+    b_nir <- brick(list.files(band_folder, pattern = "B5.TIF", full.names = TRUE))
+    message("Sensor mode: LC08")
+  } else {
+    stop("Unable to locate bands. Filenames must contain 'LC08', 'LT04' or 'LT05'")
+  }
+
+  stacked <- stack(b_green, b_nir)
+  message("stacked")
+  ndwi_res <- spectralIndices(stacked, green = 1, nir = 2, indices = "NDWI")
+  message("ndwi_res calculated")
+  
+  return(ndwi_res)
+}
